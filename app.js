e2dda3c2f9f08c8951ef505d2719944be2880aeb
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-05-16 16:13:11 -0500

    scope more

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ function scubaDive() {
 }
 
 scubaDive();
+
+
+let deadlyAnimal = "Blue-Ringed Octopus";
+
+function handleAnimal() {
+    let deadlyAnimal = "Scorpionfish";
+    console.log(deadlyAnimal);
+}
+
+handleAnimal();
+console.log(deadlyAnimal)
