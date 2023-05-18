3a8dcf32b2f5ca670cd8293223b349c9be7ca972
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-05-18 13:09:35 -0500

    returning functions

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const creature = "Common Sea Dragon";
-
-function scubaDive() {
-    const creature = "Spanish Dancer"; //A type of sea slug
-    console.log(creature);
+function makeMysteryFunc() {
+    const rand = Math.random();
+    if (rand > 0.5) {
+        return function () {
+            console.log("Congrats, I am a good function!")
+            console.log("You win a million dollars!")
+        }
+    } else {
+        return function () {
+            alert("You have been infected by a computer virus!")
+            alert("Stop trying to close this window!")
+            alert("Stop trying to close this window!")
+            alert("Stop trying to close this window!")
+            alert("Stop trying to close this window!")
+            alert("Stop trying to close this window!")
+        }
+    }
 }
-
-scubaDive();
-
-
-let deadlyAnimal = "Blue-Ringed Octopus";
-
-function handleAnimal() {
-    let deadlyAnimal = "Scorpionfish";
-    console.log(deadlyAnimal);
-}
-
-handleAnimal();
-console.log(deadlyAnimal)
