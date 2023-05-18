d86f39235a91a46c50ede75e00b8dd01832d01b8
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-05-18 13:42:19 -0500

    define lexical scope

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-function makeMysteryFunc() {
-    const rand = Math.random();
-    if (rand > 0.5) {
-        return function () {
-            console.log("Congrats, I am a good function!")
-            console.log("You win a million dollars!")
-        }
-    } else {
-        return function () {
-            alert("You have been infected by a computer virus!")
-            alert("Stop trying to close this window!")
-            alert("Stop trying to close this window!")
-            alert("Stop trying to close this window!")
-            alert("Stop trying to close this window!")
-            alert("Stop trying to close this window!")
-        }
+function parent() {
+    const num1 = 3;
+    console.log("parentFunc")
+
+    function child() {
+        const num2 = 2;
+        console.log("childFunc")
+        console.log(num1)
     }
+
+    console.log(num2);
+    child();
 }
+parent();
+
+
+
