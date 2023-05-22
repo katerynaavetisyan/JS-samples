ed441eedb8f1e1c2fcf9ae86bc7065833e614b40
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-05-22 16:12:03 -0500

    Map Practice

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const hen = {
-    name: 'Helen',
-    eggCount: 0,
-    layAnEgg() {
-        this.eggCount++;
-        return 'EGG';
-    }
-};
+const fullNames = [{ first: 'Albus', last: 'Dumbledore' }, { first: 'Harry', last: 'Potter' }, { first: 'Hermione', last: 'Granger' }, { first: 'Ron', last: 'Weasley' }, { first: 'Rubeus', last: 'Hagrid' }, { first: 'Minerva', last: 'McGonagall' }, { first: 'Severus', last: 'Snape' }];
+
+const firstNames = fullNames.map(function (person) {
+    return person.first;
+})
