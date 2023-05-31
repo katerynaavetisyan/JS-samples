c10b8c2acdd63416250b7600475d8af21ee7e0f0
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-05-31 12:25:44 -0500

    getColor Conditional Exercise

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const fullNames = [{ first: 'Albus', last: 'Dumbledore' }, { first: 'Harry', last: 'Potter' }, { first: 'Hermione', last: 'Granger' }, { first: 'Ron', last: 'Weasley' }, { first: 'Rubeus', last: 'Hagrid' }, { first: 'Minerva', last: 'McGonagall' }, { first: 'Severus', last: 'Snape' }];
+// const fullNames = [{ first: 'Albus', last: 'Dumbledore' }, { first: 'Harry', last: 'Potter' }, { first: 'Hermione', last: 'Granger' }, { first: 'Ron', last: 'Weasley' }, { first: 'Rubeus', last: 'Hagrid' }, { first: 'Minerva', last: 'McGonagall' }, { first: 'Severus', last: 'Snape' }];
 
-const firstNames = fullNames.map(function (person) {
-    return person.first;
-})
+// const firstNames = fullNames.map(function (person) {
+//     return person.first;
+// })
+
+const x = 3
+
+if (x > 3 || x == 3) {
+    console.log(x > 3)
+    console.log("if block")
+} else {
+    console.log(x < 3)
+    console.log("else block")
+}
