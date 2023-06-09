d49f5e5eb7f086ec5b6252ce719fd09d98c5637a
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-06-09 15:40:23 -0500

    balls exersise

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-let recipe = {
-  'title': 'Mole',
-  'servings': 2,
-  'ingredients': ['cumin', 'cinnamon', 'cocoa']
-};
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
-console.log(recipe.title);
-console.log('Servings: ' + recipe.servings);
-console.log('Ingredients:');
-for (var i = 0; i < recipe.ingredients.length; i++) {
-  console.log(recipe.ingredients[i]);
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+}
+draw(); {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
 }
