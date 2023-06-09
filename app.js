20db40aeff83aaa04f8c4ce8a806476400879928
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-06-09 15:36:30 -0500

    Exercises: Objects The Recipe Card

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const person = {
-  firstName: "John",
-  lastName: "Doe"
+let recipe = {
+  'title': 'Mole',
+  'servings': 2,
+  'ingredients': ['cumin', 'cinnamon', 'cocoa']
 };
 
-alert(person.firstName);
+console.log(recipe.title);
+console.log('Servings: ' + recipe.servings);
+console.log('Ingredients:');
+for (var i = 0; i < recipe.ingredients.length; i++) {
+  console.log(recipe.ingredients[i]);
+}
