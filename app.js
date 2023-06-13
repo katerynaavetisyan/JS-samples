d67c14a4154df1803ea269b9ab475f5912701169
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-06-13 16:03:33 -0500

    while loop

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-for (let i = 5; i <= 10000; i *= 10) {
+// for (let i = 5; i <= 10000; i *= 10) {
+//     console.log(i)
+// }
+
+let i = 5;
+while (i <= 10000) {
+    i *= 10;
     console.log(i)
 }
