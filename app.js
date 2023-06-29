4ff31b1e05ab46256a8504d3ffb6f9c53af45031
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-06-29 14:06:12 -0500

    Nested Arrays Exercise

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
-// false || (true && false); false
-// true || (1 + 2); true
-//     (1 + 2) || true; 3
-// true && (1 + 2); 3
-// false && (1 + 2); false
-//     (1 + 2) && true; true
-//         (32 * 4) >= 129; false
-// false !== !true; false
-// true === 4; false
-// false === (847 === '847'); false
-// false === (847 == '847'); false
-//     (!true || (!(100 / 5) === 20) || ((328 / 4) === 82)) || false; true
+// let cats = ['blue', 'kitty']
+// let dogs = ['rusty', 'wyatt']
+// cats.concat(dogs)
+
+// let colors = ['red', 'orange', 'yellow', 'green', 'blue']
+// colors.splice(2, 1, 'violet')
 
 
-//create a function converToBoolean that accepts
-//one parameter and returns its boolean value.
-//example converToBoolean(5) => true
-// converToBoolean(0) ==> false
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
 
-function converToBoolean(param) {
-    return !!param;
-}
 
-let value = 3;
-// if (value) {
-//     console.log('running code with value', value);
-// } else {
-//     console.log('I do not want to run any code');
-// }
-
-window.console.log(2);
+airplaneSeats[3][1] = "Hugo"
