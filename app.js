acb3c53395908f79136e5fac182114463f12d31c
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-06-29 14:16:46 -0500

    Object Access Exercise

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-
-
-const product = {
-    name: "Gummy Bears",
-    inStock: true,
-    price: 1.99,
-    flavors: ["grape", "apple", "cherry"]
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
 }
+
+
+const fullAddress = restaurant.address + ", " + restaurant.city + ", " + restaurant.state + " " + restaurant.zipcode;
