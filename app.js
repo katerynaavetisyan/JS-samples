460fc37c88bc5a75c47cc220cec39cce88880c01
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-06-29 14:12:17 -0500

    Object Exercise

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-// let cats = ['blue', 'kitty']
-// let dogs = ['rusty', 'wyatt']
-// cats.concat(dogs)
-
-// let colors = ['red', 'orange', 'yellow', 'green', 'blue']
-// colors.splice(2, 1, 'violet')
 
 
-const airplaneSeats = [
-    ['Ruth', 'Anthony', 'Stevie'],
-    ['Amelia', 'Pedro', 'Maya'],
-    ['Xavier', 'Ananya', 'Luis'],
-    ['Luke', null, 'Deniz'],
-    ['Rin', 'Sakura', 'Francisco']
-];
-
-
-airplaneSeats[3][1] = "Hugo"
+const product = {
+    name: "Gummy Bears",
+    inStock: true,
+    price: 1.99,
+    flavors: ["grape", "apple", "cherry"]
+}
