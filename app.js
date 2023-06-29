46000f7f827ed64beef9214f582f0324de2a9081
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-06-29 15:45:21 -0500

    working with Array references

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const restaurant = {
-    name: 'Ichiran Ramen',
-    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
-    city: 'Brooklyn',
-    state: 'NY',
-    zipcode: '11206',
-}
+// const midterms = {
+//     tom: 96,
+//     dan: 99
+// }
+// midterms.thomas = 'C+'
+// midterms['katya'] = 'A+'
 
 
-const fullAddress = restaurant.address + ", " + restaurant.city + ", " + restaurant.state + " " + restaurant.zipcode;
+// console.log('hi' === 'hi')
+// console.log(['hi', 'bye'] === ['hi', 'bye'])
+// console.log([1] === [1])
+// console.log([1] == [1])
+// console.log([] === [])
+
+// let arrayOne = [1, 2, 3]
+// let arrayTwo = [1, 2, 3]
+
+let nums = [3, 4, 5]
+let numsCopy = nums;
+console.log(nums)
+console.log(numsCopy)
+nums.push(6)
+console.log(nums)
+console.log(numsCopy)
+numsCopy.pop(6)
+console.log(nums)
+console.log(numsCopy)
