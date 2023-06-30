c11e5ebbfbfd48a2d8fcf7f54d0f1c077eb428ea
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-06-30 12:12:42 -0500

    practicing for loop

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const numOne = 10;
-const numTwo = 20;
-//using template print: Sum of 1 and 2 is 3.
-// if numbers change the result needs to change as well
+for (let i = 1; i <= 10; i += 2) {
+    console.log(i)
+    console.log("Happy Friday!");
+}
 
-console.log(`Sum of ${numOne} and ${numTwo} is ${numOne + numTwo}`)
