dd42023e58ed6020d9f00360a8922f414243572e
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-04 18:30:05 -0500

    practice for while loops

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
-// let str = 'LoL';
-// for (let i = 0; i <= 4; i++) {
-//     console.log('Outer:', i);
-//     for (let j = 0; j < str.length; j++) {
-//         console.log("    Inner:", str[j]);
-//     }
-// }
-
-// for (let i = 0; i <= 10; i++) {
-//     console.log(`i is: ${i}`)
-//     for (let j = 0; j < 4; j++) {
-//         console.log(`       j is: ${j}`)
-//     }
-// }
-
-const seatingChart = [
-    ['Anna', 'Maria', 'John'],
-    ['Artur', 'Alisa', 'Avetis'],
-    ['Katya', 'Yura', 'Maria', 'Volodya']
-]
-
-for (let i = 0; i < seatingChart.length; i++) {
-    const row = seatingChart[i];
-    console.log(`Row # ${i + 1}`);
-    for (let j = 0; j < row.length; j++) {
-        console.log(row[j]);
-    }
+let count = 0;
+while (count < 10) {
+    console.log(count)
+    count++;
 }
