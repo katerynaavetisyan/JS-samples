b1cea7c9b90550ce20dc69133aee2b7f187bcc8f
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-04 17:34:30 -0500

    loops

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
-for (let i = 1; i <= 10; i += 2) {
+for (let i = 0; i <= 6; i++) {
+    console.log(i);
+}
+for (let i = 1; i <= 20; i += 3) {
+    console.log(i, 'Perfect!');
+}
+
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
+
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
+
+for (let i = 10; i <= 1000; i *= 10) {
     console.log(i)
-    console.log("Happy Friday!");
+}
+
+for (let i = 25; i >= 0; i -= 5) {
+    console.log(i);
+}
+
+const animals = [
+    'tiger',
+    'chicken',
+    'horse',
+    'cow'
+]
+for (let i = 0; i < animals.length; i++) {
+    console.log(i, animals[i]);
+}
+console.log(animals[0])
+console.log(3)
+
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
+
+
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
 }
 
