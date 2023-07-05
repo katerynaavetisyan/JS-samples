28debf13a241ae3e08704e8475588621629b5894
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-05 13:22:27 -0500

    practice for of

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-let maximum = parseInt(prompt('Enter the maximum number!'));
-while (!maximum) {
-    maximum = parseInt(prompt("Enter a valid number!"));
-}
-const targetNum = Math.floor(Math.random() * maximum) + 1;
-console.log(targetNum);
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let guess = parseInt(prompt("Enter your first guess!"));
-let attempts = 1;
-while (parseInt(guess) !== targetNum) {
-    if (guess === 'q') break;
-    attempts++;
-    if (guess > targetNum) {
-        guess = prompt("Too high! Enter a new guess:");
-    } else {
-        guess = prompt("Too low! Enter a new guess:");
-    }
-}
-if (guess === 'q') {
-    console.log('Ok, you quit!')
-} else {
-    console.log("Congrats You Win!");
-    console.log(`You got it! It took you ${attempts} guesses`)
+for (let num of numbers) {
+    console.log(num * num)
 }
