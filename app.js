570aeba85d89df2f0b9a73f921615eb2d4e4beef
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-05 16:00:44 -0500

    To do list project

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-const testScores = {
-    bob: 32,
-    tom: 97,
-    hanna: 96,
-    kate: 98
-}
-for (let person in testScores) {
-    console.log(`${person} scored ${testScores[person]}`);
-}
-console.log(Object.keys(testScores))
-console.log(Object.values(testScores))
-console.log(Object.entries(testScores))
+let input = prompt('what would you like to do?');
 
-let total = 0;
-let scores = Object.values(testScores);
-for (let score of scores) {
-    total += score;
+
+const todos = ['Get a job', 'Make a lot of money'];
+
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log('******')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+
+        }
+        console.log('**********')
+    } else if (input === 'new') {
+        const newTodo = prompt('Ok, what is the new to do?')
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`)
+    } else if (input === 'delete') {
+        const index = parseInt(prompt('Ok, enter an index to delete:'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+
+        } else {
+            console.log('Unknown index');
+        }
+
+    }
+    input = prompt('What would you like to do??')
 }
-console.log(total / scores.length);
+console.log('Ok quit the app!')
