525fe10609780044049eb9c93f79b1c363fb45e9
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-05 14:42:00 -0500

    For... of loop practice

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const subreddits = ['books', 'cringe', 'chickens', 'funny']
+for (let i = 0; i < subreddits.length; i++) {
+    console.log(`Visit reddit.com/r/${subreddits[i]}`)
+}
 
-for (let num of numbers) {
-    console.log(num * num)
+
+for (let sub of subreddits) {
+    console.log('for...of')
+    console.log(`Visit reddit.com/r/${sub}`);
+}
+
+
+const seatingChart = [
+    ['Tom', 'Anthony', 'Maria'],
+    ['Jack', 'Tony', 'Betsy'],
+    ['Sonya', 'Bob']
+]
+
+for (let row of seatingChart) {
+    for (let student of row) {
+        console.log(student)
+    }
+}
+
+for (let char of 'Hello world') {
+    console.log(char);
 }
