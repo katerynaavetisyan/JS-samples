4c86a1df527faa27826f7584e1c0ce5b05e9334a
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-04 19:00:36 -0500

    While loop samples

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-let count = 0;
-while (count < 10) {
-    console.log(count)
-    count++;
+// let count = 0;
+// while (count <= 10) {
+//     console.log(count)
+//     count++;
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i, "while");
+// }
+
+const SECRET = 'Slava Ukraini';
+let guess = prompt('Enter the secret code...');
+while (guess !== SECRET) {
+    guess = prompt('enter the code...');
 }
+console.log('Congrats you got the secret!!!');
