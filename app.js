9b2509dc9ee44a74200e925fb74e2481267d306f
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-05 12:43:30 -0500

    Guessing Game

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-let targetNum = Math.floor(Math.random() * 10);
-let guess = Math.floor(Math.random() * 10);
-while (true) {
-    guess = Math.floor(Math.random() * 10);
-    if (guess === targetNum) {
-        console.log(`correct! Guessed: ${guess} & target was: ${targetNum}`)
-    }
-    else {
-        console.log(`Guessed ${guess}...Incorrect!`);
+let maximum = parseInt(prompt('Enter the maximum number!'));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"));
+}
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt("Too high! Enter a new guess:");
+    } else {
+        guess = prompt("Too low! Enter a new guess:");
     }
 }
-
-
-let input = prompt("Hey, say something!")
-while (true) {
-    input = prompt(input);
-    if (input.toLowerCase() === "Stop coping me")
-        break;
-
+if (guess === 'q') {
+    console.log('Ok, you quit!')
+} else {
+    console.log("Congrats You Win!");
+    console.log(`You got it! It took you ${attempts} guesses`)
 }
-console.log('Ok you win!');
