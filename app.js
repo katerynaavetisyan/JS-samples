9cfd56e51869c3e2d381e6c62e4c1a6bd7a6d50e
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-06 13:33:39 -0500

    isShortsWeather Function

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-// function isSnakeEyes(die1, die2) {
-//     if (die1 === 1 && die2 === 1) {
-//         console.log('Snake Eyes!');
-//     } else {
-//         console.log('Not Snake Eyes!');
-//     }
-// }
 
-
-function multiply(x, y) {
-    return x * y;
-
+function isShortsWeather(temperature) {
+    if (temperature >= 75) {
+        return true
+    } else {
+        return false
+    }
 }
-console.log(multiply(2, 3));
-
-multiply(2, 3) // 6
-multiply(9, 9) // 81
-multiply(5, 4) // 20
+console.log(isShortsWeather(80));
 
