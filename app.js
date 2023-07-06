c4c6c5542db8d1dcf9aadd820efd8d053ce9810a
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-06 13:22:24 -0500

    8 ways to solve  1 problem

--- app.js
+++ app.js
@@ -1,187 +1,110 @@
-
-function printHeart() {
-    console.log('<3');
-}
-printHeart()
-
-function singSong() {
-    console.log('DO');
-    console.log('RE');
-    console.log('MI');
-}
-singSong()
-
-
-function greeting() {
-    console.log('Hi');
-}
-greeting()
-
-function greet(person) {
-    console.log(`Hi, ${person}`);
-}
-greet('Tom')
-
-function welcome(firstName) {
-    console.log(`firstName is: ${firstName}`);
-}
-welcome('Kate')
-
-function rant(message) {
-    for (let i = 0; i < 3; i++) {
-        console.log(message.toUpperCase());
-    }
-}
-
-rant('I love you!')
-
-function rant(message) {
-    console.log(message.toUpperCase());
-    console.log(message.toUpperCase());
-    console.log(message.toUpperCase());
-}
-
-function greetGuest(firstName, lastName) {
-    console.log(`Hey there, ${firstName}, ${lastName[0]}.`);
-}
-
-greetGuest('Kateryna', 'Avetisyan')
-
-function repeat(str, numTimes) {
-    let result = '';
-    for (let i = 0; i < numTimes; i++) {
-        result += str;
-    }
-    console.log(result);
-}
-repeat('Happiness     ', 10)
-
-function isSnakeEyes(die1, die2) {
-    if (die1 === 1 && die2 === 1) {
-        console.log('Snake Eyes!');
-    } else {
-        console.log('Not Snake Eyes!');
-    }
-}
-
-
-function add(x, y) {
-    console.log(x + y)
-}
-add(8, 8)
-
-
-
-function adding(x, y) {
-    if (typeof x !== 'number' || typeof y !== 'number') {
-        return false;
-    }
-    let sum = x + y;
-    return sum;
-}
-
-console.log(adding(4, "tewst"));
-
-
-function multiply(x, y) {
-    return x * y;
-
-}
-console.log(multiply(5, 4));
-
-function isShortsWeather(temperature) {
-    return temperature >= 75;
-}
-let array = [3, 5, 7];
-
-function lastElement(array) {
-    return array.length ? array[array.length - 1] : null;
-}
-
-
-
-
 SOLUTION #1:
 
-function lastElement(arr) {
-    if (arr.length === 0) {
+function returnDay(num) {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    if (num < 1 || num > 7) {
         return null;
     } else {
-        return arr[arr.length - 1];
+        return days[num - 1];
     }
 }
 SOLUTION #2:
 
-function lastElement(arr) {
-    if (!arr.length) {
+function returnDay(num) {
+    const days = [null, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    if (num < 1 || num > 7) {
         return null;
+    } else {
+        return days[num];
     }
-    return arr[arr.length - 1];
-}
-
-// Or we can write it like this:
-function lastElement(arr) {
-    if (!arr.length) return null;
-    return arr[arr.length - 1];
 }
 SOLUTION #3:
 
-function lastElement(arr) {
-    if (arr.length > 0) {
-        return arr[arr.length - 1];
+function returnDay(num) {
+    if (num < 1 || num > 7) {
+        return null;
     }
-    return null;
-}
-
-// Or we can write it like this:
-function lastElement(arr) {
-    if (arr.length > 0) return arr[arr.length - 1];
-    return null;
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    return days[num - 1];
 }
 SOLUTION #4:
 
-// Using the JavaScript ternary operator syntax
-function lastElement(arr) {
-    return arr.length > 0 ? arr[arr.length - 1] : null;
-}
-
-// Another approach:
-// arr.length will be 'truthy' if it's not 0, so we can write this:
-function lastElement(arr) {
-    return arr.length ? arr[arr.length - 1] : null;
-}
-
-
-SOLUTION #1:
-
-function capitalize(word) {
-    return word[0].toUpperCase() + word.slice(1);
-}
-SOLUTION #2:
-
-// A bit more explicit solution:
-function capitalize(word) {
-    const firstLetterCapitalized = word[0].toUpperCase();
-    const restOfTheString = word.slice(1);
-    return firstLetterCapitalized + restOfTheString;
-}
-
-SOLUTION #1:
-
-function sumArray(nums) {
-    let total = 0;
-    for (let num of nums) {
-        total += num;
+function returnDay(num) {
+    if (num >= 1 && num <= 7) {
+        const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        return days[num - 1];
     }
-    return total;
+    return null;
 }
-SOLUTION #2:
+SOLUTION #5:
 
-function sumArray(nums) {
-    let total = 0;
-    for (let i = 0; i < nums.length; i++) {
-        total += nums[i];
+function returnDay(num) {
+    if (num < 1 || num > 7) {
+        return null;
     }
-    return total;
+    const days = {
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday',
+        7: 'Sunday'
+    };
+    return days[num];
 }
+SOLUTION #6:
 
+function returnDay(num) {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const day = days[num - 1];
+    // Using the JavaScript ternary operator syntax
+    return day ? day : null;
+}
+SOLUTION #7:
+
+// A long approach:
+function returnDay(num) {
+    if (num < 1 || num > 7) {
+        return null;
+    } else if (num === 1) {
+        return ('Monday');
+    } else if (num === 2) {
+        return ('Tuesday');
+    } else if (num === 3) {
+        return ('Wednesday');
+    } else if (num === 4) {
+        return ('Thursday');
+    } else if (num === 5) {
+        return ('Friday');
+    } else if (num === 6) {
+        return ('Saturday');
+    } else if (num === 7) {
+        return ('Sunday');
+    }
+}
+SOLUTION #8:
+
+// A long approach with a switch statement
+function returnDay(num) {
+    if (num >= 1 || num <= 7) {
+        switch (num) {
+            case 1:
+                return 'Monday';
+            case 2:
+                return 'Tuesday';
+            case 3:
+                return 'Wednesday';
+            case 4:
+                return 'Thursday';
+            case 5:
+                return 'Friday';
+            case 6:
+                return 'Saturday';
+            case 7:
+                return 'Sunday';
+            default:
+                return null;
+        }
+    }
+}
