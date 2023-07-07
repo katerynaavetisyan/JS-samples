ea86bdb2f3aa0579b0c5f8b7322ef32fda3c4091
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-07 11:20:46 -0500

    Last Element Exercise

--- app.js
+++ app.js
@@ -1,10 +1,55 @@
 
-function isShortsWeather(temperature) {
-    if (temperature >= 75) {
-        return true
-    } else {
-        return false
-    }
-}
-console.log(isShortsWeather(80));
+
+// function lastElement(arr) {
+//     if (arr.length === 0) {
+//         return null;
+//     } else {
+//         return arr[arr.length - 1];
+//     }
+// }
+
+let testArr = [1]
+
+console.log(testArr[testArr.length - 2]);
+
+// SOLUTION #2:
+
+// function lastElement(arr) {
+//     if (!arr.length) {
+//         return null;
+//     }
+//     return arr[arr.length - 1];
+// }
+
+// // Or we can write it like this:
+// function lastElement(arr) {
+//     if (!arr.length) return null;
+//     return arr[arr.length - 1];
+// }
+// SOLUTION #3:
+
+// function lastElement(arr) {
+//     if (arr.length > 0) {
+//         return arr[arr.length - 1];
+//     }
+//     return null;
+// }
+
+// // Or we can write it like this:
+// function lastElement(arr) {
+//     if (arr.length > 0) return arr[arr.length - 1];
+//     return null;
+// }
+// SOLUTION #4:
+
+// // Using the JavaScript ternary operator syntax
+// function lastElement(arr) {
+//     return arr.length > 0 ? arr[arr.length - 1] : null;
+// }
+
+// // Another approach:
+// // arr.length will be 'truthy' if it's not 0, so we can write this:
+// function lastElement(arr) {
+//     return arr.length ? arr[arr.length - 1] : null;
+// }
 
