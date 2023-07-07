3fb014c8c1f62605f58401b8641e7d5f7e5f43e8
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-07 13:26:45 -0500

    Function Scope

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-
-
-function returnDay(num) {
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    if (num < 1 || num > 7) {
-        return null;
-    } else {
-        return days[num - 1];
-    }
+let animal = "Giant Pacific Octopus";
+function observe() {
+    let animal = "Pajama Squid";
+    console.log(animal);
 }
+observe();
+
+const creature = "Common Sea Dragon";
+
+function scubaDive() {
+    const creature = "Spanish Dancer";
+    console.log(creature);
+}
+
+scubaDive();
+
+
+let deadlyAnimal = "Blue-Ringed Octopus";
+
+function handleAnimal() {
+    let deadlyAnimal = "Scorpionfish";
+    console.log(deadlyAnimal);
+}
+
+handleAnimal();
+console.log(deadlyAnimal)
