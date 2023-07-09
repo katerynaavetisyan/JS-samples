2145e66db2acd011a78c546a46a40a6de21e43d8
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-09 14:01:35 -0500

    Arrow Function

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-const fullNames = [{ first: 'Albus', last: 'Dumbledore' }, { first: 'Harry', last: 'Potter' }, { first: 'Hermione', last: 'Granger' }, { first: 'Ron', last: 'Weasley' }, { first: 'Rubeus', last: 'Hagrid' }, { first: 'Minerva', last: 'McGonagall' }, { first: 'Severus', last: 'Snape' }];
-
-
-const firstNames = fullNames.map(function (name) {
-    return name.first;
-});
-
-const firstNames2 = fullNames.map(name => name.first);
+const greet = name => `Hey ${name}!`;
