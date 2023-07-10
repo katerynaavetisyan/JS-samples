1253ba2c95d75bd40bce127d01a4cc48d8c73dd1
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-10 16:56:47 -0500

    Function: The Puppy Age Calculator

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-function tellFortune(jobTitle, geoLocation, partner, numKids) {
-    var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ' and married to ' +
-        partner + ' ' + ' with ' + numKids + ' kids.';
-    console.log(future);
+function calculateDogAge(age) {
+    var dogYears = 7 * age;
+    console.log("Your doggie is " + dogYears + " years old in dog years!");
 }
 
-tellFortune('bball player', 'spain', 'Shaq', 3);
-tellFortune('stunt double', 'Japan', 'Ryan Gosling', 3000);
-tellFortune('Elvis impersonator', 'Ukraine', 'The Oatmeal', 0);
+calculateDogAge(1);
