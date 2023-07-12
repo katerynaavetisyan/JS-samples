cead154e5215876153eeea5d221ed34a28312172
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-12 15:48:12 -0500

    'this' in methods

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-const myMath = {
-    PI: 3.14159,
-    square: function (num) {
-        return num * num;
-    },
-    cube: function (num) {
-        return num ** 3;
+const person = {
+    first: 'Kate',
+    last: 'Ave',
+    fullName() {
+        return `${this.first} ${this.last}`
     }
 }
-console.log(myMath.square(4));
-console.log(myMath.cube(4));
-
-const math = {
-    greet: 'Hi!',
-    add(x, y) {
-        return x + y;
-    }
-}
-console.log(math.add(5, 540));
-console.log(math.greet);
+console.log(person.fullName());
+console.log(person.first = 'Artur');
+console.log(person.fullName());
