481a191fb6b4e597e067db6fe08b172c8acc55e7
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-13 14:48:52 -0500

    the map method

--- app.js
+++ app.js
@@ -1,47 +1,14 @@
-const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-nums.forEach(function (n) {
-  console.log(n * n);
-});
-nums.forEach(function (el) {
-  if (el % 2 === 0) {
-    console.log(el);
-  }
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const doubles = numbers.map(function (num) {
+  return num * 2;
 })
+console.log(doubles);
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
-numbers.forEach(function (el) {
-  if (el % 3 === 0) {
-    console.log(el);
-    console.log('Answer');
-  }
-})
-
-numbers.forEach(function (el) {
-  console.log(el);
-  console.log("FINAL");
+const texts = ['lol', 'omg', 'ttyl'];
+const caps = texts.map(function (t) {
+  return t.toUpperCase();
 
 })
-for (let el of numbers) {
-  console.log(el);
-}
-
-const movies = [
-  {
-    title: 'Amadeus',
-    score: 99
-  },
-  {
-    title: 'Stand By Me',
-    score: 85
-  },
-  {
-    title: 'Paradise',
-    score: 95
-  }
-]
-
-movies.forEach(function (movie) {
-  console.log(`${movie.title.toUpperCase()} - ${movie.score}/100`);
-})
-
+console.log(texts);
+console.log(caps);
