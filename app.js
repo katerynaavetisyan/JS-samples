76382759ea85ab6b29c5b722580e639d10041756
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-13 14:11:28 -0500

    forEach Method

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
-try {
-  hello.toUpperCase();
-
-} catch {
-  console.log('Error!');
-}
-// hello.toUpperCase();
-console.log('After!');
-
-function yell(msg) {
-  try {
-    console.log(msg.toUpperCase().repeat(3));
-  } catch (e) {
-    console.log("Please pass a string next time!");
+const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+nums.forEach(function (n) {
+  console.log(n * n);
+});
+nums.forEach(function (el) {
+  if (el % 2 === 0) {
+    console.log(el);
   }
+})
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+numbers.forEach(function (el) {
+  if (el % 3 === 0) {
+    console.log(el);
+    console.log('Answer');
+  }
+})
+
+numbers.forEach(function (el) {
+  console.log(el);
+  console.log("FINAL");
+
+})
+for (let el of numbers) {
+  console.log(el);
 }
+
+const movies = [
+  {
+    title: 'Amadeus',
+    score: 99
+  },
+  {
+    title: 'Stand By Me',
+    score: 85
+  },
+  {
+    title: 'Paradise',
+    score: 95
+  }
+]
+
+movies.forEach(function (movie) {
+  console.log(`${movie.title.toUpperCase()} - ${movie.score}/100`);
+})
+
