8c603e427f0fac239ef454868ff40c0a6acda763
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-14 11:17:33 -0500

    Arrow Functions

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const square = (x) => {
+  return x * x;
+}
+console.log(square(63));
 
-const doubles = numbers.map(function (num) {
-  return num * 2;
-})
-console.log(doubles);
 
-const texts = ['lol', 'omg', 'ttyl'];
-const caps = texts.map(function (t) {
-  return t.toUpperCase();
+const sum = (x, y) => {
+  return x + y;
+}
+console.log(sum(5, 4));
 
-})
-console.log(texts);
-console.log(caps);
+const add = (x, y) => {
+  return x + y;
+}
+console.log(add(4, 9));
+
+
+const rollDie = () => {
+  return Math.floor(Math.random() * 5) + 1
+}
+console.log(rollDie());
+
+const square2 = num => {
+  return num * num;
+}
+console.log(square2(35));
+
+const greet = (name) => {
+  return `Hey ${name}!`;
+}
+console.log(greet('Anna'));
+
+
+const greet2 = name => `Hey ${name}!`;
+
+console.log(greet2('Maria'));
+
+
