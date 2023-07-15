e89a9e7f61408e1241a31d2a1bd46bda74c045d0
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-15 10:44:07 -0500

    filter method practice

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-const odds = nums.filter(n => {
-  return n % 2 === 1;
+
+
+const numbers = nums.filter(n => {
+  return n === 4
 })
 console.log(nums);
-console.log(odds);
-const smallNums = nums.filter(n => n < 5);
-console.log(smallNums);
+console.log(numbers);
+
