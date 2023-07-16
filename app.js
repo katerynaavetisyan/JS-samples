8e559303d525889a88662bab40889d1a6323904f
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-16 09:56:57 -0500

    the loop stop

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-function multiplyBy() {
-    num1 = document.getElementById("firstNumber").value;
-    num2 = document.getElementById("secondNumber").value;
-    document.getElementById("result").innerHTML = num1 * num2;
-}
+for (i = 0; i < 10; i++) {
+    console.log(i);
+    if (i == 5) {
 
-function divideBy() {
-    num1 = document.getElementById("firstNumber").value;
-    num2 = document.getElementById("secondNumber").value;
-    document.getElementById("result").innerHTML = num1 / num2;
+        break
+        ;
+    }
 }
