ace9210c8eead84c47057ed6f94252f0759dda4f
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-16 09:55:44 -0500

    calculate multiplication and division of two numbers

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const var_name = 'abcd';
-const n = 120;
-this[var_name] = n;
-console.log(this[var_name])
+function multiplyBy() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+}
