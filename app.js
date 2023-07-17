a65184e07a5d499b3dd00ac92d4df3e928a6b980
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-17 14:11:20 -0500

    every method and some method

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-const words = ['dog', 'cupcake', 'bag', 'wag'];
-words.some(word => {
-    return word.length > 4;
-})
-words.some(word => word[0] === 'Z');
-words.some(w => w.includes('cake'));
-console.log(words);
+const exams = [80, 98, 92, 78, 88, 90, 89, 81, 77]
+
+console.log(exams.every(score => score >= 75));
+console.log(exams.some(score => score >= 75));
