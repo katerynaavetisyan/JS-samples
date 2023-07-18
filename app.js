07e1755453c83b7e9547b3b2aaac98916cbacf7f
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-18 12:38:46 -0500

    reduce practice

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const prices = [1.99, 5.99, 4.56, 87, 54.21];
-const total = prices.reduce((total, price) => {
-    return total + price
-})
+// const prices = [1.99, 5.99, 4.56, 87, 54.21];
+// const total = prices.reduce((total, price) => {
+//     console.log('------------------')
+//     console.log('total:', total)
+//     console.log('price:', price)
+//     console.log('------------------')
+//     return total + price
+// })
 // console.log(total);
 
-let total2 = 0;
-for (let price of prices) {
-    total2 = total2 + price
-    console.log(total2);
-} console.log(total2);
+//
+
+// let total2 = 0;
+// for (let price of prices) {
+//     total2 = total2 + price
+//     console.log(total2);
+// }
+//  console.log(total2);
+
+const wordsArr = ['Hello', 'my', 'name', 'is', 'Kate'];
+
+let stringOfWords = wordsArr.reduce((allWords, words) => {
+    return allWords + ' ' + words
+
+});
+
+console.log(stringOfWords);
+//Hello my name is Kate 
