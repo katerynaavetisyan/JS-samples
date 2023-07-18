01508f25e81129a27819d8a3ceb3a07ca7a44142
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-18 11:57:54 -0500

    reduce practice

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
-// let total = 0;
-// for (let price of prices) {
-//     total += price
-// }
-// console.log(total);
-
+const prices = [1.99, 5.99, 4.56, 87, 54.21];
 const total = prices.reduce((total, price) => {
     return total + price
 })
-console.log(total);
+// console.log(total);
 
-const total2 = prices.reduce((total2, price) => total2 + price)
-console.log(total2);
-
-const minPrice = prices.reduce((min, price) => {
-    if (price < min) {
-        return price;
-    }
-    return min;
-})
-console.log(minPrice);
+let total2 = 0;
+for (let price of prices) {
+    total2 = total2 + price
+    console.log(total2);
+} console.log(total2);
