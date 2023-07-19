f18c8994e44497a77a384e9e57e4a50a7a8f138d
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-19 16:44:10 -0500

    conditional practice

--- app.js
+++ app.js
@@ -4,11 +4,35 @@
 //Password can't contain empty space. if it does log 'password can't contain spaces'
 //if password is valid log: Valid password
 
+// const password = prompt('Enter a password')
+// if (password.length < 10) {
+//     console.log('Password is too short');
+// } else if (password.indexOf(' ') >= 0) {
+//     console.log('password can not contain spaces');
+// } else {
+//     console.log('Valid password');
+// }
+
+
+//1. change your code to use nested condition
+
+
+// const password = prompt('Enter a password')
+// if (password.length < 10) {
+//     console.log('Password is too short');
+//     if (password.indexOf(' ') >= 0) {
+//         console.log('password can not contain spaces');
+//     }
+// } else {
+//     console.log('Valid password');
+// }
+
+//2. change your code to use AND oparator and only one if/else statement
+
+
 const password = prompt('Enter a password')
-if (password.length < 10) {
-    console.log('Password is too short');
-} else if (password.indexOf(' ') >= 0) {
-    console.log('password can not contain spaces');
+if (password.length < 10 && password.indexOf(' ') >= 0) {
+    console.log('Invalid password');
 } else {
     console.log('Valid password');
 }
