228b89adce72ff5aed05d0ab90c5f04e0a10978b
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-19 16:10:05 -0500

    Conditional logic - Checking if password is good

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-function greet(msg = "Hey there", person) {
-    console.log(`${msg}, ${person}!`);
-}
-console.log(greet('Hello', 'Katia'));
+//create conditional logic to validate the password.
+//password can't be shorter than 10
+//if the password is shorter than 10 log 'Password is too short'
+//Password can't contain empty space. if it does log 'password can't contain spaces'
+//if password is valid log: Valid password
 
-function greet2(person, msg = "Hey there", punc = "!") {
-    console.log(`${msg}, ${person}${punc}`);
+const password = prompt('Enter a password')
+if (password.length < 10) {
+    console.log('Password is too short');
+} else if (password.indexOf(' ') >= 0) {
+    console.log('password can not contain spaces');
+} else {
+    console.log('Valid password');
 }
-console.log(greet2("Artur", "Helioo", "!!!!!!!????"));
-
