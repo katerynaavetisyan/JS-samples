5a2a7153d7ac938e0866cc9fe40a51195302cf96
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-19 11:28:01 -0500

    'this' keyword

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-// const prices = [1.99, 5.99, 4.56, 87, 54.21];
-// const total = prices.reduce((total, price) => {
-//     console.log('------------------')
-//     console.log('total:', total)
-//     console.log('price:', price)
-//     console.log('------------------')
-//     return total + price
-// })
-// console.log(total);
-
-//
-
-// let total2 = 0;
-// for (let price of prices) {
-//     total2 = total2 + price
-//     console.log(total2);
-// }
-//  console.log(total2);
-
-const wordsArr = ['Hello', 'my', 'name', 'is', 'Kate'];
-
-let stringOfWords = wordsArr.reduce((allWords, words) => {
-    return allWords + ' ' + words
-
-});
-
-console.log(stringOfWords);
-//Hello my name is Kate 
+const person = {
+    firsName: 'Kate',
+    lastName: 'Ave',
+    fullName: function () {
+        return `${this.firsName} ${this.lastName}`
+    },
+    shoutName: function () {
+        setTimeout(() => {
+            console.log(this.fullName());
+        }, 3000)
+    }
+}
+console.log(person.fullName());
