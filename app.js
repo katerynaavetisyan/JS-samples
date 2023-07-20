d669ac06a42f9003028b2a4bf276eaefa9a925cd
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-20 16:40:40 -0500

    AND oparator and only one if/else statement

--- app.js
+++ app.js
@@ -34,24 +34,24 @@
 //2. change your code to use AND oparator and only one if/else statement
 
 
-// const password = prompt('Enter a password')
-// if (password.length > 10 && password.indexOf(' ') === -1) {
-//     console.log('Invalid password');
-// } else {
-//     console.log('Valid password');
-// }
+const password = prompt('Enter a password')
+if (password.length > 10 && password.indexOf(' ') === -1) {
+    console.log('Invalid password');
+} else {
+    console.log('Valid password');
+}
 
 
 
 /**
- * 1. create a function that accepts an array 
+ * 1. create a function that accepts an array
  * and logs every element of the array using FOR LOOP
  *
- * 
+ *
  * 2. do same task but replace FOR LOOP with
  * one for array methods(think what method is best)
- * 
- * 3.Write a function 'fill' that creates a new array with given values 
+ *
+ * 3.Write a function 'fill' that creates a new array with given values
  *  example: const result = fill(5, 'Kate')
  * console.log(result)
  * ['Kate', 'Kate', 'Kate', 'Kate', 'Kate']
@@ -73,20 +73,20 @@
 // }
 // console.log(printElements(['car', 'house', 'business']))
 
-//3
-function fill(times, word) {
-    //create new empty array
-    let result = [];
-    //create for loop that adds `word` `times` number of times
-    for (let i = 0; i < times; i++) {
-        result.push(word)
-    }
-    //return updated new array
-    return result
-}
+// //3
+// function fill(times, word) {
+//     //create new empty array
+//     let result = [];
+//     //create for loop that adds `word` `times` number of times
+//     for (let i = 0; i < times; i++) {
+//         result.push(word)
+//     }
+//     //return updated new array
+//     return result
+// }
 
-const newArr = fill(5, '  Kate')
-console.log(newArr)
+// const newArr = fill(5, '  Kate')
+// console.log(newArr)
 
 // function fill(times, word) {
 //     const result = [];
