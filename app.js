1818de3f4c06e8f067a22cf59216f623f979a570
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-20 12:41:12 -0500

    password exercise

--- app.js
+++ app.js
@@ -17,22 +17,22 @@
 //1. change your code to use nested condition
 
 
-// const password = prompt('Enter a password')
-// if (password.length < 10) {
-//     console.log('Password is too short');
-//     if (password.indexOf(' ') >= 0) {
-//         console.log('password can not contain spaces');
-//     }
-// } else {
-//     console.log('Valid password');
-// }
+const password = prompt('Enter a password')
+if (password.length < 10) {
+    console.log('Password is too short');
+    if (password.indexOf(' ') >= 0) {
+        console.log('password can not contain spaces');
+    }
+} else {
+    console.log('Valid password');
+}
 
 //2. change your code to use AND oparator and only one if/else statement
 
 
-const password = prompt('Enter a password')
-if (password.length < 10 && password.indexOf(' ') >= 0) {
-    console.log('Invalid password');
-} else {
-    console.log('Valid password');
-}
+// const password = prompt('Enter a password')
+// if (password.length < 10 && password.indexOf(' ') >= 0) {
+//     console.log('Invalid password');
+// } else {
+//     console.log('Valid password');
+// }
