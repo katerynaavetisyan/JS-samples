1fb7412d67473888c26e3550ff1c05907bdfd8f6
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-21 16:13:17 -0500

    rest params

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const feline = { legs: 4, family: 'Felidae' }
-const canine = { family: 'Caninoe', furry: true }
+// function sum() {
+//     return arguments.reduce((total, el) => total + el)
+// }
+// console.log(sum(25, 652, 5632));
+// console.log(Math.min);
 
-const dogs = { ...canine, isPet: true }
-console.log(dogs);
+function sum(...nums) {
+    return nums.reduce((total, el) => total + el)
+}
+console.log(sum(35, 525, 656, 62, 25, 21, 3, 3, 5, 6, 59, 6));
+console.log(sum(5, 6));
 
-const lion = { ...feline, genus: 'Panthera' }
-console.log(lion);
-
-const catDog = { ...feline, ...canine }
-console.log(catDog);
+function raceResults(gold, silver, ...everyoneElse) {
+    console.log(`GOLD MEDAL GOES TO: ${gold}`);
+    console.log(`SILVER MEDAL GOES TO: ${silver}`);
+    console.log(`AND THANKS TO EVERYONE ELSE:${everyoneElse}`);
+}
+console.log(raceResults('Kate', 'Artur', 'David', 'Tom'));
