7409f05772247d2c9492a5627c0d49e35096f0df
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-21 16:22:10 -0500

    Rest / Spread Operator Exercise

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-// function sum() {
-//     return arguments.reduce((total, el) => total + el)
-// }
-// console.log(sum(25, 652, 5632));
-// console.log(Math.min);
-
-function sum(...nums) {
-    return nums.reduce((total, el) => total + el)
+function filterOutOdds() {
+    var nums = Array.prototype.slice.call(arguments);
+    return nums.filter(function (num) {
+        return num % 2 === 0
+    });
 }
-console.log(sum(35, 525, 656, 62, 25, 21, 3, 3, 5, 6, 59, 6));
-console.log(sum(5, 6));
-
-function raceResults(gold, silver, ...everyoneElse) {
-    console.log(`GOLD MEDAL GOES TO: ${gold}`);
-    console.log(`SILVER MEDAL GOES TO: ${silver}`);
-    console.log(`AND THANKS TO EVERYONE ELSE:${everyoneElse}`);
-}
-console.log(raceResults('Kate', 'Artur', 'David', 'Tom'));
