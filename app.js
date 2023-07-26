f6cdc9a57874a3504eebfc24c2162e3916d64461
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-07-26 18:52:04 -0500

    Destructuring Array

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
-const user = {
-    email: 'harvey@gmail.com',
-    password: 'dsfhbstnbdfb4758',
-    firstName: 'Harvey',
-    lastName: 'Milk',
-    born: 1930,
-    // died: 1978,
-    city: 'San Francisco',
-    state: 'California'
-}
-const { email, firstName, lastName, city, state } = user;
 
-console.log(email);
-console.log(lastName);
-console.log(city);
+let getDetails = ([name_of_an_employee, employee_id, employee_designation]) => {
+    console.log(
+        `Employee's name : ${name_of_an_employee} , Employee's ID: ${employee_id}, Employee's Designation: ${employee_designation}`
+    );
+};
 
-
-const { born: birthYear, died: deathYear = 'N/A' } = user;
-console.log(birthYear);
-
-function fullName(user) {
-    return `${user.firstName} ${user.lastName}`
-}
-console.log(fullName(user));
-
-
-function fullName(user) {
-    const { firstName, lastName } = user;
-    return `${firstName} ${lastName}`
-}
-console.log(fullName(user));
-
-function fullName({ firstName, lastName }) {
-    return `${firstName} ${lastName}`
-}
-
+getDetails(employee_details);
