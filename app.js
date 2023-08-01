13da07d3e1b896761d61eb9deb905f26beec6169
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-01 12:22:12 -0500

    The Lifetime Supply Calculator

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-function calcGeometry(radius) {
-    var circumference = Math.PI * 2 * radius;
-    console.log("The circumference is " + circumference);
-    var area = Math.PI * radius * radius;
-    console.log("The area is " + area);
+function calculateSupply(age, numPerDay) {
+    var maxAge = 100;
+    var totalNeeded = (numPerDay * 365) * (maxAge - age);
+    var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+    console.log(message);
 }
+
+calculateSupply(28, 36);
+calculateSupply(28, 2.5);
+calculateSupply(28, 400);
+
