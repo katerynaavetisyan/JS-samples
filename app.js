26499636c69e675754640ea7368cf69f0588f731
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-01 12:31:21 -0500

    obj

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-function calculateSupply(age, numPerDay) {
-    var maxAge = 100;
-    var totalNeeded = (numPerDay * 365) * (maxAge - age);
-    var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
-    console.log(message);
-}
-
-calculateSupply(28, 36);
-calculateSupply(28, 2.5);
-calculateSupply(28, 400);
-
+const obj = {
+    id: 5,
+    name: 'San Francisco',
+    [getKey('enabled')]: true,
+};
