b3c73155c95e4835897ecb48d6b8f7bb79ba75dc
katerynaavetisyan <katerynaavetisyan@gmail.com>
2023-08-03 15:27:18 -0500

    The Fortune Teller

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const obj = {
-    id: 5,
-    name: 'San Francisco',
-    [getKey('enabled')]: true,
-};
+function tellFortune(jobTitle, geoLocation, partner, numKids) {
+    var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ' and married to ' +
+        partner + ' ' + ' with ' + numKids + ' kids.';
+    console.log(future);
+}
+
+tellFortune('bball player', 'spain', 'Shaq', 3);
+tellFortune('stunt double', 'Japan', 'Ryan Gosling', 3000);
+tellFortune('Elvis impersonator', 'Russia', 'The Oatmeal', 0);
